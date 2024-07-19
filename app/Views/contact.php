5e09cb03a23a0968f5f1483d028fc5910d7f690e
PriyankaMitech <priyanka.mitech@gmail.com>
2024-07-19 17:23:23 +0530

    Ui Contact Page Changes

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -1,7 +1,9 @@
 
 <?php include 'header.php'; ?>
 
+
 	<!-- Hamburger Menu -->
+	<div class="full-contact-page">
 
 	<div class="hamburger_menu">
 		<div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
@@ -51,95 +53,94 @@
 		</div>
 	</div>
 
-	<div class="container contact_container">
-		
-		<!-- Map Container -->
-		<div class="benefit pb-5">
-			<div class="container">
-				<div class="row benefit_row">
-					<div class="col-lg-6 ">
-						<a href="link1.html"> <!-- Add your desired link here -->
-							<div class="benefit_item d-flex flex-row align-items-center">
-								<div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
-								<div class="benefit_content">
-									<h6>Shipping Traking Details</h6>
-									<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-6 ">
-						<a href="<?=base_url();?>raiseaticket"> <!-- Add your desired link here -->
-							<div class="benefit_item d-flex flex-row align-items-center">
-								<div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
-								<div class="benefit_content">
-									<h6>Raise Ticket</h6>
-									<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="container contact_container contact-page">
+    
+    <!-- Map Container -->
+	<div class="benefit pb-5">
+    <div class="container">
+        <div class="row benefit_row">
+            <div class="col-lg-6">
+                <a href="link1.html"> <!-- Add your desired link here -->
+                    <div class="benefit_item d-flex flex-row align-items-center shipping-traking-blog">
+                        <div class="benefit_icon">
+                            <img src="<?=base_url(); ?>public/images/shipping.png" alt="Shipping Tracking Icon" >
+                        </div>
+                        <div class="benefit_content">
+                            <h6>Shipping Tracking Details</h6>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <a href="<?=base_url();?>raiseaticket"> <!-- Add your desired link here -->
+                    <div class="benefit_item d-flex flex-row align-items-center raise-ticket-blog">
+                        <div class="benefit_icon">
+                            <img src="<?=base_url(); ?>public/images/ticket.webp" alt="Raise Ticket Icon" >
+                        </div>
+                        <div class="benefit_content">
+                            <h6>Raise Ticket</h6>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 
-	
 
-		<!-- Contact Us -->
+    <!-- Contact Us -->
+    <div class="row">
+        <div class="col-lg-6 contact_col">
+            <div class="contact_contents">
+                <h1>Contact Us</h1>
+                <p>There are many ways to contact us. You may drop us a line, give us a call or send an email, choose what suits you the most.</p>
+                <div>
+                    <p>(800) 686-6688</p>
+                    <p>info.deercreative@gmail.com</p>
+                </div>
+                <div>
+                    <p>mm</p>
+                </div>
+                <div>
+                    <p>Open hours: 8.00-18.00 Mon-Fri</p>
+                    <p>Sunday: Closed</p>
+                </div>
+            </div>
 
-		<div class="row">
+            <!-- Follow Us -->
+            <div class="follow_us_contents">
+                <h1>Follow Us</h1>
+                <ul class="social d-flex flex-row">
+                    <li><a href="#" style="background-color: #3a61c9"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#" style="background-color: #41a1f6"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#" style="background-color: #fb4343"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                    <li><a href="#" style="background-color: #8f6247"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
 
-			<div class="col-lg-6 contact_col">
-				<div class="contact_contents">
-					<h1>Contact Us</h1>
-					<p>There are many ways to contact us. You may drop us a line, give us a call or send an email, choose what suits you the most.</p>
-					<div>
-						<p>(800) 686-6688</p>
-						<p>info.deercreative@gmail.com</p>
-					</div>
-					<div>
-						<p>mm</p>
-					</div>
-					<div>
-						<p>Open hours: 8.00-18.00 Mon-Fri</p>
-						<p>Sunday: Closed</p>
-					</div>
-				</div>
-
-				<!-- Follow Us -->
-
-				<div class="follow_us_contents">
-					<h1>Follow Us</h1>
-					<ul class="social d-flex flex-row">
-						<li><a href="#" style="background-color: #3a61c9"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-						<li><a href="#" style="background-color: #41a1f6"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						<li><a href="#" style="background-color: #fb4343"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-						<li><a href="#" style="background-color: #8f6247"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-
-			</div>
-
-			<div class="col-lg-6 get_in_touch_col">
-				<div class="get_in_touch_contents">
-					<h1>Get In Touch With Us!</h1>
-					<p>Fill out the form below to recieve a free and confidential.</p>
-					<form action="post">
-						<div>
-							<input id="input_name" class="form_input input_name input_ph" type="text" name="name" placeholder="Name" required="required" data-error="Name is required.">
-							<input id="input_email" class="form_input input_email input_ph" type="email" name="email" placeholder="Email" required="required" data-error="Valid email is required.">
-							<input id="input_website" class="form_input input_website input_ph" type="url" name="name" placeholder="Website" required="required" data-error="Name is required.">
-							<textarea id="input_message" class="input_ph input_message" name="message"  placeholder="Message" rows="3" required data-error="Please, write us a message."></textarea>
-						</div>
-						<div>
-							<button id="review_submit" type="submit" class="red_button message_submit_btn trans_300" value="Submit">send message</button>
-						</div>
-					</form>
-				</div>
-			</div>
-
-		</div>
+        <div class="col-lg-6 get_in_touch_col">
+            <div class="get_in_touch_contents">
+                <h1>Get In Touch With Us!</h1>
+                <p>Fill out the form below to receive a free and confidential.</p>
+                <form action="post">
+                    <div>
+                        <input id="input_name" class="form_input input_name input_ph" type="text" name="name" placeholder="Name" required="required" data-error="Name is required.">
+                        <input id="input_email" class="form_input input_email input_ph" type="email" name="email" placeholder="Email" required="required" data-error="Valid email is required.">
+                        <input id="input_website" class="form_input input_website input_ph" type="url" name="website" placeholder="Website" required="required" data-error="Website is required.">
+                        <textarea id="input_message" class="input_ph input_message" name="message" placeholder="Message" rows="3" required data-error="Please, write us a message."></textarea>
+                    </div>
+                    <div>
+                        <button id="review_submit" type="submit" class="red_button message_submit_btn trans_300" value="Submit">Send Message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 	</div>
+</div>
 
 	<!-- Newsletter -->
 
